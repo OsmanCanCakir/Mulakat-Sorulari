caf76eb68fcebb088772c5d79eb110f81bd4f624
OsmanCanCakir <93882084+OsmanCanCakir@users.noreply.github.com>
2023-05-17 14:44:22 +0300

    Mülakat Sorularu
    Mülakat Sorularını içermektedir.

---
+++ MulakatSorulari/Soru1/Program.cs
@@ -0,0 +1,33 @@
+﻿
+using System;
+public class Solution
+{
+    public  bool KopyaIcerirMi(int[] nums)
+    {
+        List<int> set = new List<int>();  
+        foreach (int num in nums)
+        {
+            if (set.Contains(num))
+            {
+                return true;
+            }
+            set.Add(num);
+        }
+        return false;
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        int[] nums = { 1,1,1,3,3,4,3,2,4,2 }; //İstediğiniz şekilde nums array'ini değiştirebilirsiniz.
+
+        //int[] nums = { 1, 2, 3, 4 };
+        //int[] nums = { 1, 1, 1, 3, 3, 4, 3, 2, 4, 2};
+
+        Solution solution = new Solution();
+        bool result = solution.KopyaIcerirMi(nums);
+        Console.WriteLine("Çıktı: " + result);
+    }
+}
---
+++ MulakatSorulari/Soru2/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+public class Solution
+{
+    public int MaksimumKar(int[] prices, int buyDay)
+    {
+        int maxProfit = 0;
+        int minPrice = prices[buyDay];
+        int sellDay = 0;
+
+        for (int i = buyDay + 1; i < prices.Length; i++)
+        {
+            if (prices[i] - minPrice > maxProfit)
+            {
+                maxProfit = prices[i] - minPrice;
+                sellDay = i;
+            }
+        }
+        //İndeks karşılığı gün karşılığına dönüştür.
+        Console.WriteLine("En iyi satış günü: " + (sellDay + 1));
+        return maxProfit;
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        bool exit = false;
+        while(!exit)
+        {
+            Console.WriteLine("Lütfen hisse senedi fiyatlarını giriniz... (virgülle ayırarak):");
+            string input = Console.ReadLine();
+            string[] priceStrings = input.Split(',');
+            int[] prices = new int[priceStrings.Length];
+
+            for (int i = 0; i < priceStrings.Length; i++)
+            {
+                int price;
+                if (int.TryParse(priceStrings[i], out price))
+                {
+                    prices[i] = price;
+                }
+                else
+                {
+                    Console.WriteLine("Geçersiz giriş! Lütfen tamsayı değerleri girin.");
+                    return;
+                }
+            }
+
+            Console.WriteLine("Alım yapılacak günü giriniz:");
+            int buyDay;
+            if (!int.TryParse(Console.ReadLine(), out buyDay) || buyDay < 1 || buyDay >= prices.Length)
+            {
+                Console.WriteLine("Hatalı giriş! Lütfen geçerli bir gün indeksi girin.");
+                return;
+            }
+            //1. gün 0. indekse karşılık gelecek
+            buyDay--;
+            Solution solution = new Solution();
+            int maxProfit = solution.MaksimumKar(prices, buyDay);
+
+            Console.WriteLine("Maksimum kar: " + maxProfit);
+            Console.WriteLine("Devam etmek için bir tuşa basın veya 'Q' tuşuna basarak çıkın...");
+
+            ConsoleKeyInfo keyInfo = Console.ReadKey();
+            if (keyInfo.KeyChar == 'q' || keyInfo.KeyChar == 'Q')
+            {
+                exit = true;
+            }
+        }
+        
+    }
+}
